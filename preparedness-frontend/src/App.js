a26a7cac2b3e51931bd07c88e3964dce29b12694
LEARN Academy <lanyuke@hotmail.com>
2023-06-11 00:46:00 -0700

    finish front end

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+
+  const [name, setName] = useState('')
+
+  const handleReset = () => {
+    setName('');
+  }
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
-    <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
-      <div className="form">
-        <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+    <div 
+    className="backimg"
+    >
+      <div className="entire-content">
+
+        <h1>Preparedness Assessment</h1>
+
+        <div className="form">
+
+          <div className="input">
+            <Label for="name">Enter your name</Label>
+            <Input
+            type='text'
+            value={name}
+            onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="buttonsCon">
+            <ModalComponent name={name}/>
+            <Button
+              type="button"
+              className="btn btn-primary buttons"
+              onClick={handleReset}
+            >Reset</Button>
+          </div>
+
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
       </div>
     </div>
   )
